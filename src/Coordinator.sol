199447b828b9b85abb70231e884c01fa4c6e8233
wrinkled <wrinkledeth@gmail.com>
2022-08-29 22:18:09 -0400

    tests completed

--- src/Coordinator.sol
+++ src/Coordinator.sol
@@ -27,8 +27,11 @@ contract Coordinator is Ownable {
     /// Mapping of Ethereum Address => DKG Phase 3 Justifications
     mapping(address => bytes) public justifications;
 
-    /// List of registered Ethereum keys (used for conveniently fetching data)
+    // List of registered Ethereum keys (used for conveniently fetching data)
     address[] public participants;
+
+    //! Added participants map for more efficient "onlyRegistered"
+    mapping(address => bool) public participant_map;
 
     /// The duration of each phase
     uint256 public immutable PHASE_DURATION;
@@ -44,10 +47,11 @@ contract Coordinator is Ownable {
 
     /// A registered participant is one whose pubkey's length > 0
     modifier onlyRegistered() {
-        require(
-            userState[msg.sender] == UserState.Registered,
-            "you are not registered!"
-        );
+        // require(
+        //     userState[msg.sender] == UserState.Registered,
+        //     "you are not registered!"
+        // );
+        require(participant_map[msg.sender], "you are not registered!");
         _;
     }
 
@@ -63,51 +67,61 @@ contract Coordinator is Ownable {
         // owner = msg.sender;
     }
 
-    /// Kickoff function which starts the counter (INITIALIZE)
-    function start() external onlyWhenNotStarted onlyOwner {
-        // require(msg.sender == owner, "only owner may start the DKG");
-        startBlock = block.number;
-    }
+    // ! Start done via initialize() in randcast
+    // /// Kickoff function which starts the counter (INITIALIZE)
+    // function start() external onlyWhenNotStarted onlyOwner {
+    //     // require(msg.sender == owner, "only owner may start the DKG");
+    //     startBlock = block.number;
+    // }
 
     // ! Allowlist (DKG Nodes) done via initialize() in randcast
-    /// The administrator must allowlist an addrss for participation in the DKG
-    function allowlist(address user) external onlyWhenNotStarted onlyOwner {
-        // require(msg.sender == owner, "only owner may allowlist users");
+    // /// The administrator must allowlist an addrss for participation in the DKG
+    // function allowlist(address user) external onlyWhenNotStarted onlyOwner {
+    //     // require(msg.sender == owner, "only owner may allowlist users");
 
-        require(
-            userState[user] == UserState.CannotRegister,
-            "user is already allowlisted"
-        );
-        userState[user] = UserState.CanRegister;
-    }
+    //     require(
+    //         userState[user] == UserState.CannotRegister,
+    //         "user is already allowlisted"
+    //     );
+    //     userState[user] = UserState.CanRegister;
+    // }
 
     //! Register done via initialize() in randcast
     /// This function ties a DKG participant's on-chain address with their BLS Public Key
-    function register(bytes calldata blsPublicKey) external onlyWhenNotStarted {
-        require(
-            userState[msg.sender] == UserState.CanRegister,
-            "user is not allowlisted or has already registered"
-        );
+    // function register(bytes calldata blsPublicKey) external onlyWhenNotStarted {
+    //     require(
+    //         userState[msg.sender] == UserState.CanRegister,
+    //         "user is not allowlisted or has already registered"
+    //     );
 
-        participants.push(msg.sender);
-        keys[msg.sender] = blsPublicKey;
+    //     participants.push(msg.sender);
+    //     keys[msg.sender] = blsPublicKey;
 
-        // the user is now registered
-        userState[msg.sender] = UserState.Registered;
-    }
+    //     // the user is now registered
+    //     userState[msg.sender] = UserState.Registered;
+    // }
 
     //! New initialize code here (replaces start() and register())
-    struct Member {
-        address node_address;
-        // uint256 node_index; // index of node within group
-        bytes blsPublicKey;
-    }
+    // struct Member {
+    //     address node_address;
+    //     // uint256 node_index; // index of node within group
+    //     bytes blsPublicKey;
+    // }
 
-    function initialize(Member[] calldata)
+    function initialize(address[] calldata nodes, bytes[] calldata publicKeys)
         external
         onlyWhenNotStarted
+        // group.epoch # do we really need this?
         onlyOwner
-    {}
+    {
+        for (uint256 i = 0; i < nodes.length; i++) {
+            participant_map[nodes[i]] = true;
+            participants.push(nodes[i]);
+            keys[nodes[i]] = publicKeys[i];
+        }
+
+        startBlock = block.number;
+    }
 
     /// Participant publishes their data and depending on the phase the data gets inserted
     /// in the shares, responses or justifications mapping. Reverts if the participant
